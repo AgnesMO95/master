33a2d9f85086da6c560067ddb708f0fb145ae933
Agnes Marie Ødegård Ockernahl <amockern@stud.ntnu.no>
2022-05-07 17:32:50 +0200

    added changes to _app.tsx, to get it work with MUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@mui/material/styles'
+import { CssBaseline } from '@mui/material'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import theme from '../styles/theme'
+import createEmotionCache from '../lib/createEmotionCache'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+//client-side cahce, shared for the whole session of the user in the browser
+const clientSideEmotionCache = createEmotionCache()
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
 }
 
-export default MyApp
+function App(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
+}
+
+export default App
