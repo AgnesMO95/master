4facf45fb7ef82e09097432bb80cb17c6a0695ae
Agnes Marie Ødegård Ockernahl <amockern@stud.ntnu.no>
2022-05-13 15:07:10 +0200

    created a post api with flask, to upload multiple images.

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from email.mime import base
+from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/detect", methods=['POST'])
+def detect():
+    files =  request.files.getlist('files') #files or image
+    #files = request.files['images']
+    for file in files:
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(basepath, 'uploads', file.filename)
+        file.save(file_path)
+
+    return jsonify({"response": "files uploaded successfully"} )
 
 if __name__ == "__main__":
     app.run(debug=True)
