0d58223c8dd8280080ebb21b07db54daf026d8a2
Agnes Marie Ødegård Ockernahl <amockern@stud.ntnu.no>
2022-05-15 15:14:08 +0200

    appended results for each file in a dictionary

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,25 @@
 from flask import Flask, request, jsonify
 import os
+from yolov4darknet.detect import runModel
 
 app = Flask(__name__)
 
-@app.route("/detect", methods=['POST'])
+@app.route('/detect', methods=['POST'])
 def detect():
-    files =  request.files.getlist('files') #files or image
-    #files = request.files['images']
-    for file in files:
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(basepath, 'uploads', file.filename)
-        file.save(file_path)
+    if request.method == 'POST':
+        files =  request.files.getlist('files') #files or image
+        #files = request.files['images']
+        results = []
+        for file in files:
+            basepath = os.path.dirname(__file__)
+            file_path = os.path.join(basepath, 'uploads', file.filename)
+            file.save(file_path)
+            print(file_path)
+            result = runModel(file_path)
+            results.append(result)
 
-    return jsonify({"response": "files uploaded successfully"} )
+        return jsonify(results )
+    return None
 
 if __name__ == "__main__":
     app.run(debug=True)
