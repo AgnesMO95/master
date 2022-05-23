7e5fd9a6e935809ef99018de8af09eaf9167b361
Agnes Marie Ødegård Ockernahl <amockern@stud.ntnu.no>
2022-05-23 18:40:58 +0200

    Modified component to take in list of images as props instead

--- components/layout/TitlebarBelowImageList.tsx
+++ components/layout/TitlebarBelowImageList.tsx
@@ -4,7 +4,14 @@ import ImageListItemBar from '@mui/material/ImageListItemBar'
 import Image from 'next/image'
 import theme from '../../styles/theme'
 
-export default function TitlebarBelowImageList() {
+interface Props {
+  itemData: {
+    img: string
+    count: string
+  }[]
+}
+
+export default function TitlebarBelowImageList(props: Props) {
   const handleOnClick = async () => {
     console.log('hei')
   }
@@ -29,17 +36,17 @@ export default function TitlebarBelowImageList() {
         },
       }}
     >
-      {itemData.map(item => (
-        <ImageListItem key={item.author}>
+      {props.itemData.map(item => (
+        <ImageListItem key={item.img}>
           <img
             src={`${item.img}?w=248&fit=crop&auto=format`}
             //srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
+            alt={item.img}
             loading="lazy"
             onClick={handleOnClick}
           />
           <ImageListItemBar
-            title={<span>Count: {item.title}</span>}
+            title={<span>Count: {item.count}</span>}
             subtitle={item.img}
             position="below"
           />
@@ -48,66 +55,3 @@ export default function TitlebarBelowImageList() {
     </ImageList>
   )
 }
-
-const itemData = [
-  {
-    img: '/12330-6675-20361.png',
-    title: '700',
-    author: '@bkristastucchio',
-  },
-  {
-    img: '/12330-6675-20361.png',
-    title: '650',
-    author: '@rollelflex_graphy726',
-  },
-  {
-    img: '/12330-6675-20361.png',
-    title: '730',
-    author: '@helloimnik',
-  },
-  {
-    img: '/12330-6675-20361.png',
-    title: '683',
-    author: '@nolanissac',
-  },
-  {
-    img: '/12330-6675-20361.png',
-    title: '712',
-    author: '@hjrc33',
-  },
-  {
-    img: '/12330-6675-20361.png',
-    title: '705',
-    author: '@arwinneil',
-  },
-  {
-    img: '/12330-6675-20361.png',
-    title: '699',
-    author: '@tjdragotta',
-  },
-  {
-    img: '/12330-6675-20361.png',
-    title: '620',
-    author: '@katie_wasserman',
-  },
-  {
-    img: '/12330-6675-20361.png',
-    title: '645',
-    author: '@silverdalex',
-  },
-  {
-    img: '/12330-6675-20361.png',
-    title: '800',
-    author: '@shelleypauls',
-  },
-  {
-    img: '/12330-6675-20361.png',
-    title: '500',
-    author: '@peterlaster',
-  },
-  {
-    img: '/12330-6675-20361.png',
-    title: '555',
-    author: '@southside_customs',
-  },
-]
