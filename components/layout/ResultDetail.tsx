6336d800823b963abbf815242d26c4bb667be0ca
Agnes Marie Ødegård Ockernahl <amockern@stud.ntnu.no>
2022-05-25 15:27:46 +0200

    functionality for rendering multipe preview of one osteoclasts with confidence, and added delete button to all items

--- components/layout/ResultDetail.tsx
+++ components/layout/ResultDetail.tsx
@@ -1,6 +1,17 @@
-import { Container, Grid } from '@mui/material'
-import { Fragment } from 'react'
+import {
+  Box,
+  Card,
+  CardContent,
+  Container,
+  Grid,
+  IconButton,
+  ListItemSecondaryAction,
+  Tooltip,
+  Typography,
+} from '@mui/material'
+import { Fragment, useState } from 'react'
 import styled from '@emotion/styled'
+import DeleteIcon from '@mui/icons-material/Delete'
 
 const StyledImage = styled.img`
   //width: 100px;
@@ -11,7 +22,7 @@ const StyledImage = styled.img`
   //width: 100px;
   /* top: 2000;
   left: 2000; */
-  margin-left: -4000px;
+  margin-left: -4300px;
   margin-top: -4100px;
 `
 
@@ -31,7 +42,7 @@ const detection = [
   },
   {
     label: 'Osteoclast',
-    confidence: '0.3',
+    confidence: '0.34',
     x: 39,
     y: 10,
     w: 20,
@@ -39,7 +50,7 @@ const detection = [
   },
   {
     label: 'Osteoclast',
-    confidence: '0.3',
+    confidence: '0.37',
     x: 39,
     y: 10,
     w: 20,
@@ -47,7 +58,7 @@ const detection = [
   },
   {
     label: 'Osteoclast',
-    confidence: '0.3',
+    confidence: '0.4',
     x: 39,
     y: 10,
     w: 20,
@@ -55,7 +66,7 @@ const detection = [
   },
   {
     label: 'Osteoclast',
-    confidence: '0.3',
+    confidence: '0.42',
     x: 39,
     y: 10,
     w: 20,
@@ -63,7 +74,7 @@ const detection = [
   },
   {
     label: 'Osteoclast',
-    confidence: '0.3',
+    confidence: '0.46',
     x: 39,
     y: 10,
     w: 20,
@@ -71,7 +82,7 @@ const detection = [
   },
   {
     label: 'Osteoclast',
-    confidence: '0.3',
+    confidence: '0.5',
     x: 39,
     y: 10,
     w: 20,
@@ -80,6 +91,8 @@ const detection = [
 ]
 
 const ResultDetail = (props: Props) => {
+  const [isHover, setHover] = useState(false)
+
   return (
     <Fragment>
       <Container sx={{ py: 8 }} maxWidth="lg">
@@ -87,21 +100,48 @@ const ResultDetail = (props: Props) => {
           <Grid item xs={8}>
             <img src={props.image} alt={props.title} width={'100%'} />
           </Grid>
-          <Grid item xs={2}>
-            <div
-              style={{
-                width: 64,
-                height: 64,
-                overflow: 'hidden',
-                //position: 'absolute',
-              }}
-            >
-              <StyledImage
-                src={props.image}
-                alt={props.title}
-                // style={{ top: '450', left: '1320' }}
-              />
-            </div>
+          <Grid item xs={4}>
+            <Container>
+              <Grid container spacing={4} justifyContent="center">
+                {detection.map(item => (
+                  <Grid item key={item.confidence} spacing={4}>
+                    {/* {isHover && <DeleteIcon />} */}
+                    <Tooltip
+                      disableFocusListener
+                      disableTouchListener
+                      title="Delete"
+                      placement="top-end"
+                    >
+                      <Card>
+                        <IconButton>
+                          <DeleteIcon />
+                        </IconButton>
+                        <div
+                          style={{
+                            width: 100,
+                            height: 100,
+                            overflow: 'hidden',
+                            //position: 'absolute',
+                          }}
+                        >
+                          <StyledImage
+                            src={props.image}
+                            alt={props.title}
+                            onMouseEnter={() => setHover(true)}
+                            onMouseLeave={() => setHover(false)}
+                            // style={{ top: '450', left: '1320' }}
+                          />
+                        </div>
+
+                        <CardContent sx={{ flexGrow: 1 }}>
+                          <Typography>{item.confidence}</Typography>
+                        </CardContent>
+                      </Card>
+                    </Tooltip>
+                  </Grid>
+                ))}
+              </Grid>
+            </Container>
           </Grid>
         </Grid>
       </Container>
