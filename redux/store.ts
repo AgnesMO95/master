a63626815b9223e1397cdc581aad129ed0e03ab2
Agnes Marie Ødegård Ockernahl <amockern@stud.ntnu.no>
2022-05-29 15:46:49 +0200

    added imageListReducer

--- redux/store.ts
+++ redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import imageFileListReducer from './images/imageSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    imageFileList: imageFileListReducer,
+  },
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
