6c9a94df525f64e20b002303fd87143665b8c041
Agnes Marie Ødegård Ockernahl <amockern@stud.ntnu.no>
2022-05-30 13:15:02 +0200

    added empty pages to test routing

---
+++ pages/upload.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+//import styles from '../../styles/Home.module.css'
+import styled from '@emotion/styled'
+
+const Div = styled.div`
+  margin: 40px;
+`
+
+const upload: NextPage = () => {
+  return <div></div>
+}
+
+export default upload
