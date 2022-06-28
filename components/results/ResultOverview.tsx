89ff51be821c87b9d081cc9ca021e819cf491c4c
Agnes Marie Ødegård Ockernahl <amockern@stud.ntnu.no>
2022-06-28 22:21:12 +0200

    deleted comments and uneccesary code

--- components/results/ResultOverview.tsx
+++ components/results/ResultOverview.tsx
@@ -1,13 +1,8 @@
-import ImageList from '@mui/material/ImageList'
-import ImageListItem from '@mui/material/ImageListItem'
-import ImageListItemBar from '@mui/material/ImageListItemBar'
 import Button from '@mui/material/Button'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
 import Stack from '@mui/material/Stack'
-import Image from 'next/image'
-import theme from '../../styles/theme'
 import { useRouter } from 'next/router'
 import Card from '@mui/material/Card'
 import CardActions from '@mui/material/CardActions'
@@ -17,17 +12,9 @@ import { useAppSelector, useAppDispatch } from '../../redux/hooks'
 
 import Grid from '@mui/material/Grid'
 
-interface Props {
-  itemData: {
-    img: string
-    count: string
-  }[]
-}
-
-export default function TitlebarBelowImageList(props: Props) {
+export default function TitlebarBelowImageList() {
   const router = useRouter()
   const images = useAppSelector(state => state.imageFileList.images)
-  const urls = useAppSelector(state => state.imageFileList.urls)
   const predictions = useAppSelector(state => state.prediction.predictions)
   console.log(images)
   const dispatch = useAppDispatch()
@@ -79,7 +66,6 @@ export default function TitlebarBelowImageList(props: Props) {
         </Container>
       </Box>
       <Container sx={{ py: 8 }} maxWidth="lg">
-        {/* End hero unit */}
         <Grid container spacing={4}>
           {images.map(item => (
             <Grid item key={item.name} xs={12} sm={6} md={4} lg={3}>
@@ -92,12 +78,6 @@ export default function TitlebarBelowImageList(props: Props) {
               >
                 <CardMedia
                   component="img"
-                  sx={
-                    {
-                      // 16:9
-                      //pt: '56.25%',
-                    }
-                  }
                   image={URL.createObjectURL(item)}
                   alt="random"
                 />
@@ -117,49 +97,12 @@ export default function TitlebarBelowImageList(props: Props) {
                   >
                     Detailed results
                   </Button>
-                  {/* <Button size="small">Edit</Button> */}
                 </CardActions>
               </Card>
             </Grid>
           ))}
         </Grid>
       </Container>
-      {/* <ImageList
-        cols={4}
-        sx={{
-          width: {
-            xs: theme.breakpoints.up('xs'),
-            sm: theme.breakpoints.up('sm'),
-            md: theme.breakpoints.up('md'),
-            lg: theme.breakpoints.up('lg'),
-            xl: theme.breakpoints.up('xl'),
-          },
-          height: {
-            xs: theme.breakpoints.up('xs'),
-            sm: theme.breakpoints.up('sm'),
-            md: theme.breakpoints.up('md'),
-            lg: theme.breakpoints.up('lg'),
-            xl: theme.breakpoints.up('xl'),
-          },
-        }}
-      >
-        {props.itemData.map(item => (
-          <ImageListItem key={item.img}>
-            <img
-              src={`${item.img}?w=248&fit=crop&auto=format`}
-              //srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-              alt={item.img}
-              loading="lazy"
-              onClick={() => handleOnClick(item.img)}
-            />
-            <ImageListItemBar
-              title={<span>Count: {item.count}</span>}
-              subtitle={item.img}
-              position="below"
-            />
-          </ImageListItem>
-        ))}
-      </ImageList> */}
     </main>
   )
 }
