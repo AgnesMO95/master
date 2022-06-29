b73793feff810a9562ca77f0087a7136de79dcb9
Agnes Marie Ødegård Ockernahl <amockern@stud.ntnu.no>
2022-06-29 13:55:51 +0200

    modified to by dynamic, sending the image name from the url as props to the component

--- pages/results/[img].tsx
+++ pages/results/[img].tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from 'next'
 import ResultDetail from '../../components/results/ResultDetail'
+import { useRouter } from 'next/router'
 
 const resultDetails: NextPage = () => {
-  return (
-    <ResultDetail image="/12330-6675-20361.png" title="/12330-6675-20361.png" />
-  )
+  const router = useRouter()
+  const image = router.query.img as string
+  return <ResultDetail image={image} />
 }
 
 export default resultDetails
